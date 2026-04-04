96f3443c8b182ac2a34089b7ff52b7d18d63b15a
Imran Nayem <nayem.imran52@gmail.com>
2026-04-04 21:07:00 +0600

    json,api fetching need to be done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
-
+import { useState } from "react";
 
 import NavBar from "./component/NavBar";
 import Banner from "./component/Banner";
 import StatsSection from "./component/StatsSection";
+import Models from "./component/Models";
+import Footer from "./component/Footer";
+import Cart from "./component/Cart";
+import { ToastContainer } from 'react-toastify'
+
+const getModels = async () => {
+  const res = await fetch("/models.json");
+  return res.json();
+};
+
+const modelPromise = getModels();
+
 function App() {
+  const [activeTab, setActiveTab] = useState("model");
+  const [carts, setCarts] = useState([]);
+
   return (
     
       <>
       <NavBar/>
       <Banner/>
       <StatsSection/>
+
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-box justify-center bg-transparent">
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab rounded-full w-40"
+          aria-label="Models"
+          onClick={() => setActiveTab("model")}
+          defaultChecked
+        />
+        <input
+          type="radio"
+          name="my_tabs_1"
+          className="tab rounded-full w-40"
+          aria-label={`Cart (${carts.length})`}
+          onClick={() => setActiveTab("cart")}
+        />
+      </div>
+
+      {activeTab === "model" && <Models modelPromise={modelPromise} carts={carts} setCarts={setCarts}/>}
+
+      {activeTab === "cart" && <Cart  carts={carts} setCarts={setCarts}/>}
+      <Footer/>
+       <ToastContainer/>
       </>
 
       
